ace22f23ecf17c4fc1617acb03466b3898ef3733
Zachary Goshen (CTR) <zachary.goshen@erpinternational.com>
2022-02-01 13:01:26 -0500

    Rename Spring Boot app to WorkoutBuddyApplication

--- src/main/java/com/zachgoshen/workoutbuddy/WorkoutBuddyApplication.java
+++ src/main/java/com/zachgoshen/workoutbuddy/WorkoutBuddyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkouttrackerApplication {
+public class WorkoutBuddyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkouttrackerApplication.class, args);
+		SpringApplication.run(WorkoutBuddyApplication.class, args);
 	}
 
 }
